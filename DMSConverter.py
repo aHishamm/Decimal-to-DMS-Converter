d4002f2b4eb40d740ed3e318146ee4ca97cf28c9
Abdulrahman Hisham <201610191@aust.ae>
2020-08-23 14:07:45 +0400

    completed toDMS() function

--- DMSConverter.py
+++ DMSConverter.py
@@ -1,10 +1,30 @@
 import math 
 def toDMS(coordinate): 
-    absoluteVal = abs(coordinate)
+    coordinate = coordinate.replace(" ","")
+    coordinateList = coordinate.split(",")
+    DMSList = []
+    if float(coordinateList[0]) > 0: 
+        NSParam = 'N'
+    else: 
+        NSParam = 'S'
+    if float(coordinateList[1]) > 0: 
+        EWParam = 'E'
+    else: 
+        EWParam = 'W' 
+    absoluteVal = abs(float(coordinateList[0]))
     degreesVal = math.floor(absoluteVal)
-    minutesNTruncated = (absoluteVal - degreesVal) * 60 
+    minutesNTruncated = (absoluteVal - degreesVal) * 60
     minutesVal = math.floor(minutesNTruncated)
     secondsVal = round((minutesNTruncated - minutesVal) * 60)
-    return str(degreesVal)+"°"+str(minutesVal)+"'"+str(secondsVal)
+    DMSList.append(str(degreesVal)+"°"+str(minutesVal)+"'"+str(secondsVal)+'"'+NSParam)
+    absoluteVal = abs(float(coordinateList[1]))
+    degreesVal = math.floor(absoluteVal)
+    minutesNTruncated = (absoluteVal - degreesVal) * 60
+    minutesVal = math.floor(minutesNTruncated)
+    secondsVal = round((minutesNTruncated - minutesVal) * 60)
+    DMSList.append(str(degreesVal)+"°"+str(minutesVal)+"'"+str(secondsVal)+'"'+EWParam)
+    return " ".join(DMSList)
 
-print(toDMS(24.210959))
+#print(toDMS(24.210959))
+#Please enter the latlong coordinate in this format --> "lat, long"
+ConvertedCoordinate = toDMS(input("Please enter the latlong coordinate to be converted: (should be comma separated)\n"))
